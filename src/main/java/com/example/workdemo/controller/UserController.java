31e7099a9503321ab9b5065676c543f4776f026f
cx <sherlocknnz@163.com>
2023-01-01 18:42:23 +0800

    .

--- src/main/java/com/example/workdemo/controller/UserController.java
+++ src/main/java/com/example/workdemo/controller/UserController.java
@@ -1,17 +1,23 @@
 package com.example.workdemo.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.workdemo.entity.Sys_User;
-import com.example.workdemo.mapper.UserMapper;
 import com.example.workdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -78,5 +84,69 @@ public class UserController {
 //        res.put("total", total);
 //        return res;
 //    }
+    /**
+     * 导出接口
+     */
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws Exception {
+        // 从数据库查询出所有的数据
+        List<Sys_User> list = userService.list();
+        // 通过工具类创建writer 写出到磁盘路径
+//        ExcelWriter writer = ExcelUtil.getWriter(filesUploadPath + "/用户信息.xlsx");
+        // 在内存操作，写出到浏览器
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        //自定义标题别名
+        writer.addHeaderAlias("username", "用户名");
+        writer.addHeaderAlias("password", "密码");
+        writer.addHeaderAlias("nickname", "昵称");
+        writer.addHeaderAlias("email", "邮箱");
+        writer.addHeaderAlias("phone", "电话");
+        writer.addHeaderAlias("address", "地址");
+        writer.addHeaderAlias("create_time", "创建时间");
+        writer.addHeaderAlias("avatarUrl", "头像");
+
+        // 一次性写出list内的对象到excel，使用默认样式，强制输出标题
+        writer.write(list, true);
+
+        // 设置浏览器响应的格式
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("用户信息", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
+
+    }
+    /**
+     * excel 导入
+     * @param file
+     * @throws Exception
+     */
+    @PostMapping("/import")
+    public Boolean imp(MultipartFile file) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        // 方式1：(推荐) 通过 javabean的方式读取Excel内的对象，但是要求表头必须是英文，跟javabean的属性要对应起来
+//        List<User> list = reader.readAll(User.class);
+
+        // 方式2：忽略表头的中文，直接读取表的内容
+        List<List<Object>> list = reader.read(1);
+        List<Sys_User> users = CollUtil.newArrayList();
+        for (List<Object> row : list) {
+            Sys_User sys_user= new Sys_User();
+            sys_user.setUsername(row.get(0).toString());
+            sys_user.setPassword(row.get(1).toString());
+            sys_user.setNickname(row.get(2).toString());
+            sys_user.setEmail(row.get(3).toString());
+            sys_user.setPhone(row.get(4).toString());
+            sys_user.setAddress(row.get(5).toString());
+            users.add(sys_user);
+        }
+
+        userService.saveBatch(users);
+        return true;
+    }
 
 }
--- src/main/java/com/example/workdemo/entity/Sys_User.java
+++ src/main/java/com/example/workdemo/entity/Sys_User.java
@@ -4,10 +4,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Data
 @TableName(value = "sys_user")
+@ToString
 public class Sys_User {
     @TableId(type = IdType.AUTO)
     private Integer id;
@@ -18,4 +23,6 @@ public class Sys_User {
     private String email;
     private String phone;
     private String address;
+    @ApiModelProperty
+    private LocalDateTime create_time;
 }
