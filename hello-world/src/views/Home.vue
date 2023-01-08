22bd3979f7eb101749075ac89708e6ee09c8f63b
cx <sherlocknnz@163.com>
2023-01-08 12:51:08 +0800

    1

--- hello-world/src/views/Home.vue
+++ hello-world/src/views/Home.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
+    <el-row :gutter="10" style="margin-bottom: 40px">
+      <el-col :span="6">
+        <el-card style="color: #409EFF">
+          <div><i class="el-icon-user-solid">用户总数</i></div>
+          <div style="padding: 10px 0;text-align: center;font-weight: bold">
+              <el-tag type="primary" style="font-size: 20px">100</el-tag>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card style="color: #67C23A">
+          <div><i class="el-icon-money">销售总量</i></div>
+          <div style="padding: 10px 0;text-align: center;font-weight: bold">
+            100
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card style="color: #E6A23C">
+          <div><i class="el-icon-bank-card">收益总额</i></div>
+          <div style="padding: 10px 0;text-align: center;font-weight: bold">
+            100
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card style="color: #909399">
+          <div><i class="el-icon-s-shop">门店总数</i></div>
+          <div style="padding: 10px 0;text-align: center;font-weight: bold">
+            100
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
     <el-row>
       <el-col :span="12">
         <div id="main" style="width: 500px; height: 400px"></div>
@@ -33,6 +68,13 @@ export default {
         subtext: '趋势图',
         left: 'center'
       },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
       xAxis: {
         type: 'category',
         data: ["第一季度", "第二季度", "第三季度", "第四季度"]
@@ -42,12 +84,24 @@ export default {
       },
       series: [
         {
+          name: "星巴克",
           data: [],
           type: 'line'
         },
         {
+          name: "星巴克",
           data: [],
           type: 'bar'
+        },
+        {
+          name: "瑞幸",
+          data: [],
+          type: 'bar'
+        },
+        {
+          name: "瑞幸",
+          data: [],
+          type: 'line'
         }
       ]
     };
@@ -67,8 +121,10 @@ export default {
       },
       series: [
         {
+          name: "星巴克",
           type: 'pie',
           radius: '60%',
+          center: ['25%', '50%'],
           label:{            //饼图图形上的文本标签
             normal:{
               show:true,
@@ -89,6 +145,35 @@ export default {
               shadowColor: 'rgba(0, 0, 0, 0.5)'
             }
           }
+        },
+        {
+          name: "瑞幸",
+          type: 'pie',
+          radius: '50%',
+          center: ['75%', '50%'],
+          label:{            //饼图图形上的文本标签
+            normal:{
+              show:true,
+              position:'inner', //标签的位置
+              textStyle : {
+                fontWeight : 300 ,
+                fontSize : 14,    //文字的字体大小
+                color: "#fff"
+              },
+              formatter:'{d}%'
+            }
+          },
+          data: [{name: "第一季度", value: 5},
+            {name: "第二季度", value: 3},
+            {name: "第三季度", value: 2},
+            {name: "第四季度", value: 8},],  // 填空
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
         }
       ]
     };
@@ -101,6 +186,8 @@ export default {
       // option.xAxis.data = res.data.x
       option.series[0].data = res.data
       option.series[1].data = res.data
+      option.series[2].data = [5,3,2,8]
+      option.series[3].data = [5,3,2,8]
       // 数据准备完毕之后再set
       myChart.setOption(option);
 
